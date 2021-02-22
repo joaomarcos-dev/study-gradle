88eb78c68817c5b98dcef84b729eadb9eb8e3d45
Joao Marcos R de Carvalho <jc808853@hpeautos.com.br>
2021-02-21 21:41:00 -0300

    change Class implementation for an empty args execution

--- gradle02/src/main/java/com/pack/Class.java
+++ gradle02/src/main/java/com/pack/Class.java
@@ -2,6 +2,12 @@ package com.pack;
 public class Class{
 	
 	public static void main(String [] args){
+
+		if(args.length == 0){
+			System.out.println("No  augend or addend");
+			return;
+		}
+		
 		//Not handling any type related exceptions
 		System.out.println(
 			sum(
