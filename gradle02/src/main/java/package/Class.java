638ae51f31c082e7dce78d353fbb322f782ac067
Joao Marcos R de Carvalho <jc808853@hpeautos.com.br>
2021-02-21 21:00:06 -0300

    add the gradle02 test with the built in java plugin

---
+++ gradle02/src/main/java/package/Class.java
@@ -0,0 +1,15 @@
+public class Class{
+	
+	public static void main(String [] args){
+		//Not handling any type related exceptions
+		System.out.println(
+			sum(
+				Integer.parseInt(args[0]),
+				Integer.parseInt(args[1])
+				)
+			);
+	}
+	public static int sum(int a, int b){
+		return a+b;
+	}
+}
